7968fbb40fb06c78a22bea53c697e31aa5e73d0b
Kevin Chung <43365809+KevinYunChung@users.noreply.github.com>
2021-03-10 17:46:14 +0100

    Added CommonBase

---
+++ CommonBase/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿//@BaseCode
+using System;
+
+namespace CommonBase.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void CheckArgument(this object src, string argName)
+        {
+            if (src == null)
+                throw new ArgumentNullException(argName);
+        }
+    }
+}
